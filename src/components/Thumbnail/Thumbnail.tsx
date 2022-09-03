cf23d52dc3135c67cc5934ad593491e9ede85267
simonljus <simon.ljus94@gmail.com>
2022-09-03 11:14:02 +0200

    text to thumbnail

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import type {Document as DocumentDB, Image as ImageDB} from "@prisma/client"
+import styles from './Thumbnail.module.scss'
 type ThumbnailProps={
     document: DocumentDB & {image: ImageDB}
 }
 const Thumbnail = ({document}: ThumbnailProps) =>{
     return <> 
     <Image objectFit="cover" layout="fill" alt={document.name}  src={document.image.url} />
-    <p className="text"></p>
+    <p className={styles.text}>{document.name}</p>
     </>
 }
 export default Thumbnail;
