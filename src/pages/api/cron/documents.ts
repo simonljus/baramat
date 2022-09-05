63874a5710b829a1c0f6a3ef9303e816690f0b7c
simonljus <simon.ljus94@gmail.com>
2022-09-05 08:30:49 +0200

    postgres

--- src/pages/api/cron/documents.ts
+++ src/pages/api/cron/documents.ts
@@ -4,6 +4,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Document as DocumentKoket } from '../../../models/koket';
 import { DocumentService } from '../../../server/services/documentService';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method !== 'POST'){
+        res.status(405).send({ message: 'Only POST' })
+        return
+    }
+    const {password} =req.body || {}
+    if(!password){
+        res.status(400).send({ message: 'credentials missing' })
+        return
+    }
+    if(!process.env.CRON_PASSWORD){
+        res.status(500).send({ message: 'invalid config' })
+        return
+    }
+    if(password !== process.env.CRON_PASSWORD ){
+        res.status(400).send({ message: 'not authorized' })
+        return
+    }
     //Find the absolute path of the json directory
     const jsonDirectory = path.join(process.cwd(), 'json');
     //Read the json data file data.json
