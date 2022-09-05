4bc0fb8bb6c923364372b0ae544a09f2ef55b510
simonljus <simon.ljus94@gmail.com>
2022-09-05 08:36:18 +0200

    env schema opyional

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,12 +6,14 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  POSTGRES_DATABASE_URL: z.string().url().optional(),
+  DATABASE_URL: z.string().url().optional(),
+  CRON_PASSWORD:z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  NEXTAUTH_SECRET: z.string().optional(),
+  NEXTAUTH_URL: z.string().url().optional(),
+  DISCORD_CLIENT_ID: z.string().optional(),
+  DISCORD_CLIENT_SECRET: z.string().optional(),
 });
 
 /**
